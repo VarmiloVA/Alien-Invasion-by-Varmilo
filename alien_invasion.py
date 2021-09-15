10685c7ef016cf89b35f89484474a2be0e378348
varmilo <daniel.chichkov@gmail.com>
2021-09-15 16:30:10 +0200

    Configurada la resolución y color de fondo

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,16 +9,22 @@ class AlienInvasion:
         """Inicializa el juego y crea recursos."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((1920, 1080))
         pygame.display.set_caption("Alien Invasion")
+
+        # Configura el color de fondo
+        self.bg_color = (30, 10, 230)
 
     def run_game(self):
         """Inicia el bucle principal para el juego."""
         while True:
             #Busca eventos de teclado y ratón
-            for event in pygame.get():
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            #Redibuja la pantalla en cada paso por el bucle
+            self.screen.fill(self.bg_color)
             
             #Hace visible la última pantalla dibujada.
             pygame.display.flip()
