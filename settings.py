78892b1575402d69153d78a9a5b324522656f12e
varmilo <daniel.chichkov@gmail.com>
2021-09-15 16:35:30 +0200

    Añadido el archivo settings al proyecto

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Una clase para guardar toda la configuración de alien invasion"""
+
+    def __init__(self):
+        """Inicializa la configuraciónd el juego"""
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (30, 10, 230)
