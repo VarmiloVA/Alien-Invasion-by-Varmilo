02f8b1a907900d307628daca94bb5b1f06664d93
varmilo <daniel.chichkov@gmail.com>
2021-09-15 16:00:52 +0200

    Primeros pasos para crear Alien Invasion

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    'Clase general para gestionar los recursos y el comportamiento del juego.'
+
+    def __init__(self):
+        """Inicializa el juego y crea recursos."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Inicia el bucle principal para el juego."""
+        while True:
+            #Busca eventos de teclado y ratón
+            for event in pygame.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            #Hace visible la última pantalla dibujada.
+            pygame.display.flip()
