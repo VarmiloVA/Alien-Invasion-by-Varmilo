e37baa833658451479e1d0ea24254df180b4443c
varmilo <daniel.chichkov@gmail.com>
2021-09-24 17:43:46 +0200

    Añadido de manera comentada el código para poner el juego en pantalla completa

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,10 +13,14 @@ class AlienInvasion:
 
         self.settings = Settings()
 
+        # Modo pantalla completa
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
         self.screen = pygame.display.set_mode(
             (self.settings.setting_width, self.settings.setting_height)
-            )
-
+        )
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
