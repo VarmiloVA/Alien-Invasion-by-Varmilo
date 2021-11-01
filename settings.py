abe4280a52d8822cc2e2479235be45ab64683458
Varmilo <va87@localhost.localdomain>
2021-11-01 22:24:50 +0100

    Pequeños ajustes para que el juego corra bien en ParrotHomeOS

--- settings.py
+++ settings.py
@@ -9,12 +9,12 @@ class Settings:
         self.bg_color = (93, 118, 142)
 
         #Apaño termporal para la velocidad que hay que cambiar
-        self.ship_speed = 0.8
+        self.ship_speed = 2.5
         self.ship_horizontal_speed = self.ship_speed
         self.ship_vertical_speed = self.ship_speed
 
         #Configuración de las balas
-        self.bullet_speed = 1.0
+        self.bullet_speed = 4
         self.bullet_width = 3
         self.bullet_height = 6
         self.max_bullet_num = 4
